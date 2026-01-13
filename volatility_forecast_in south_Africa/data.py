15e5b3addacdc8a29a9fc53c2ac0d78e15b51d2b
Nababa Tee <nababatee@gmail.com>
2026-01-13 16:34:53 +0100

    progress

---
+++ volatility_forecast_in south_Africa/data.py
@@ -0,0 +1,154 @@
+"""
+This module contains all code used to interact with the AlphaVantage API
+and  a SQLite database.
+"""
+#import necessary libaries
+import os
+import requests
+import pandas as pd
+from dotenv import load_dotenv
+
+# Load environment variables from .env into the environment
+load_dotenv()
+
+
+class AlphavantageApi:
+    def __init__(self, api_key = os.getenv("alpha_vantage")):
+         # Store API key as a private attribute using name mangling
+        self.__api_key = api_key
+
+        if not self.__api_key:
+            raise ValueError("Alpha Vantage API key not found")
+
+    # create a function data get data from alpha_vantage api and returns a data frame
+    def get_daily(self, ticker, output_size="full"): 
+        """Get daily time series of an equity from AlphaVantage API.
+
+        Parameters
+        ----------
+        ticker : str
+            The ticker symbol of the equity.
+        output_size : str, optional
+            Number of observations to retrieve. "compact" returns the
+            latest 100 observations. "full" returns all observations for
+            equity. By default "full".
+
+        Returns
+        -------
+        pd.DataFrame
+            Columns are 'open', 'high', 'low', 'close', and 'volume'.
+            All are numeric.
+        """
+        data_type = "json"
+
+        url =  (
+        "https://www.alphavantage.co/query?"
+        "function=TIME_SERIES_DAILY&"
+        f"symbol={ticker}&"
+        f"outsize={output_size}&"
+        f"datatype={data_type}&"
+        f"apikey={self.__api_key}"
+    )
+        # Send request to API 
+        response = requests.get(url=url)
+        # Extract JSON data from response 
+        response_data = response.json()
+        if "Time Series (Daily)" not in response_data.keys():
+            raise Exception(
+                f"invalid API Call. check that ticker symbol {ticker} is correct"
+            )
+        # Check if there's been an error
+        response_code = response.status_code
+
+        # Read data into DataFrame
+        stock_data = response_data["Time Series (Daily)"]
+        df = pd.DataFrame.from_dict(stock_data, orient="index", dtype=float)
+        df.index.name = "date"
+
+        # Convert index to `DatetimeIndex` named "date" 
+        df.index = pd.to_datetime(df.index)
+        
+
+        # Remove numbering from columns (8.1.15)
+        df.columns = [c.split(". ")[1] for c in df.columns]
+
+        # Return DataFrame
+        return df
+    
+
+
+
+
+class SQLRepository:
+    def __init__(self, connection):
+
+        self.connection = connection
+        
+
+    def insert_table(self, table_name, records, if_exists="fail"):
+        """Insert DataFrame into SQLite database as table
+
+        Parameters
+        ----------
+        table_name : str
+        records : pd.DataFrame
+        if_exists : str, optional
+            How to behave if the table already exists.
+
+            - 'fail': Raise a ValueError.
+            - 'replace': Drop the table before inserting new values.
+            - 'append': Insert new values to the existing table.
+
+            Dafault: 'fail'
+
+        Returns
+        -------
+        dict
+            Dictionary has two keys:
+
+            - 'transaction_successful', followed by bool
+            - 'records_inserted', followed by int
+        
+        """
+        n_inserted = records.to_sql(
+            name=table_name, con=self.connection, if_exists=if_exists
+            )
+        return {
+            "transaction_successful": True,
+            "records_inserted": n_inserted
+        }
+    
+
+
+    
+    def read_table(self, table_name, limit=None):
+        """Read table from database.
+
+        Parameters
+        ----------
+        table_name : str
+            Name of table in SQLite database.
+        limit : int, None, optional
+            Number of most recent records to retrieve. If `None`, all
+            records are retrieved. By default, `None`.
+
+        Returns
+        -------
+        pd.DataFrame
+            Index is DatetimeIndex "date". Columns are 'open', 'high',
+            'low', 'close', and 'volume'. All columns are numeric.
+        """
+        # Create SQL query (with optional limit)
+        if limit:
+            sql = f"SELECT * FROM '{table_name}' LIMIT {limit}"
+        else:
+            sql = f"SELECT * FROM '{table_name}'"
+        
+
+        # Retrieve data, read into DataFrame
+        df = pd.read_sql(
+            sql=sql, con=self.connection, parse_dates=["date"], index_col="date"
+        )
+        
+        # Return DataFrame
+        return df
